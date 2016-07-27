15a2e67be20c8e3d19e2ed6d9923b86af7d3ac86
suman <suman@rhost.localdomain>
2016-07-27 13:06:34 -0400

    Updating script file and CodeBook

--- CodeBook.md
+++ CodeBook.md
@@ -9,23 +9,43 @@ As per the project our purpose was to procude a clean and tidy data sets by merg
 ## Variables of Interest
 The varailbes realted to mean and standard deviation for 3-axial linear acceleration and 3-axial angular velocity were subseted from the whole dataset (i.e. traning and test).
 
-## Process of Data Cleaning and Data transformation
-As per the project our purpose was to procude a clean and tidy data sets by merging the traning and test data sets so that it can be used for futher insight analysis.
+## Labeling data set with descriptive variable names.
+The variables obtained from features.txt can be explained as:
+- Leading t refers to time and f for frequency measurements.
+- mean() referes to mean value
+- std() refres to standard deviation
+- Body = related to body movement.
+- Gravity = acceleration of gravity
+- Acc = accelerometer measurement
+- Gyro = gyroscopic measurements
+- Jerk = sudden movement acceleration
+- Mag = magnitude of movement
 
-## Variables of Interest
-The varailbes realted to mean and standard deviation for 3-axial linear acceleration and 3-axial angular velocity were subseted from the whole dataset (i.e. traning and test).
+The variables are mapped to the descriptive names as below by selecting values for mean and standard deviation:
+- t to time
+- f to frequency
+- mean to Mean
+- std to StandardDeviation
+- Acc to Accelerometer 
+- Gyro to Gyroscope
+- Mag to Magnitude
+- BodyBody to Body
+
+e.g. tBodyAcc-mean()-X is mapped to timeBodyAccelerometerMean-X
+Similar process iterates for other variables.
 
 ## Process of Data Cleaning and Data transformation
 The cleaning and transformation of the data set is carried out by the script file **run_analysis.R**. The overall process carried out in the script file is summarize as below:
 
-1. Downloads the dataset to workspace.
-2. Extracts activities and feature info and load it.
-3. Loads the test and traning data and extract data columns related to mean and standard deviation.
-4. Tidy the column names i.e tBodyAcc-mean()-X to tBodyAcc-Mean-X using grep and gsub.
-5. Loads the activity and subject data for each test and traning data sets
+1. Downloads the dataset to workspace and extract if not already present and extracted respetively.
+2. Extracts feature and activities info and load it from files features.txt and activity_labels.txt respetively.
+3. Tidy the feature e.g. tBodyAcc-mean()-X to timeBodyAccelerometerMean-X and using grep and gsub for features mean and standard deviation.
+4. Loads the test and traning data and extract data columns related to mean and standard deviation from files y_*.txt (where * implies test and train). 
+5. Loads the activity and subject data for each test and traning data sets from files subject_*.txt (where * implies test and train).
 6. Merges the activity and subject datas with respecitve test and traning data sets..
-7. Merges the test and traning data 
-8. Provides the column names for the data.
+7. Merges the test and traning data.
+8. Provides the column names for the data i.e "Subjects" for subjects and "Activity" fro activiteis and rest the names obtained in step 3.
 8. Converts the activity and subjects of final data set as factors for further cleaning.
-9. The converted dataset obtained in step 7 is melted using the id's for subjects and activites
-10. Then the data are again transformed to independent tidy data set with the average of each variable for each activity and each subject. 
+9. The resulted dataset obtained is then melted using the id's for subjects and activites.
+10. Then the data are again transformed to independent tidy data set with the average of each variable for each activity and each subject.
+11. Finally the cleaned and transformed data set are saved to file tidyData.txt
--- run_analysis.R
+++ run_analysis.R
@@ -15,66 +15,61 @@
 #### load the packages
 library("dplyr")
 library("data.table")
+library("reshape2")
 
+## The file name for the dataset to download
+filename <- "/R/workspace/dc-week4/dataset.zip"
 
-### download the data
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, "dataset.zip", method="curl")
+## Download the dataset if not in workspace
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
 
+## Extract the downloaded compressed dataset if not already present on work space
+if (!file.exists("/R/workspace/dc-week4/uci_har_dataset")) { 
+  unzip(filename, exdir='/R/workspace/dc-week4/uci_har_dataset') 
+}
 
 ### Start tidying data.
+
+## Read tthe features dataset and store it
 features <- read.table("/R/workspace/dc-week4/uci_har_dataset/features.txt", header=FALSE)
 
-### Know the how the data look like
-str(features)
-
-#cohere the variables wanted from factors to character
+## Cohere the variables wanted from factors to character
 features[,2] <- as.character(features[,2])
-
-### Know the how the data look like ( Verify )
-str(features)
 
 ### Load the activities performed
 activities <- read.table("/R/workspace/dc-week4/uci_har_dataset/activity_labels.txt", header=FALSE)
 
-### Know the how the data look like
-str(activities)
-
-### Know the how the data look like ( Verify )
+## Cohere the variables wanted from factors to character
 activities[,2] <- as.character(activities[,2])
 
-### Know the how the data look like ( Verify )
-str(activities)
-
-
-### Extract features with mean and std
-selectedFeature <- grep("*mean*|*std*", features[,2], value=TRUE)
-
-### Know the how the data look like ( Verify )
-str(selectedFeature)
-
-### Clean the features name
-selectedFeature <- grep("*mean*|*std*", features[,2], value=TRUE)
+### Extract features with mean and std and Clean the features name
+selectedFeature <- grep("mean\\(\\)|std\\(\\)", features[,2], value=TRUE)
 selectedFeature <- gsub('-mean', "Mean", selectedFeature)
 selectedFeature <- gsub('-std', 'Std', selectedFeature)
-selectedFeature <- gsub('[()]', '', selectedFeature)
+selectedFeature <- gsub('\\(\\)', '', selectedFeature)
+selectedFeature <- gsub('^t', 'time', selectedFeature)
+selectedFeature <- gsub('^f', 'time', selectedFeature)
+selectedFeature <- gsub("Acc", "Accelerometer", selectedFeature)
+selectedFeature <- gsub("Gyro", "Gyroscope", selectedFeature)
+selectedFeature <- gsub("Mag", "Magnitude",selectedFeature)
+selectedFeature <- gsub("BodyBody", "Body", selectedFeature)
 
-### Know the how the data look like ( Verify )
-str(selectedFeature)
 
 ### Load the traning data set and clean it.
 
 # Get the index of the features to be selected
-listFeatures <- grep("*mean*|*std*", features[,2])
+listFeatures <- grep("mean\\(\\)|std\\(\\)", features[,2])
 
-### Extract only the data of intrest
+### Extract only the data of intrest for test data
 test <- read.table("/R/workspace/dc-week4/uci_har_dataset/test/X_test.txt")[listFeatures]
 activitiesTest <- read.table("/R/workspace/dc-week4/uci_har_dataset/test/y_test.txt")
 subjectsTest <- read.table("/R/workspace/dc-week4/uci_har_dataset/test/subject_test.txt")
 testData <- cbind(subjectsTest, activitiesTest, test)
 
-
-### Extract only the data of intrest
+### Extract only the data of intrest for traning data
 train <- read.table("/R/workspace/dc-week4/uci_har_dataset/train/X_train.txt")[listFeatures]
 activitiesTrain <- read.table("/R/workspace/dc-week4/uci_har_dataset/train/y_train.txt")
 subjectsTrain <- read.table("/R/workspace/dc-week4/uci_har_dataset/train/subject_train.txt")
@@ -90,7 +85,15 @@ combineData <- rbind(trainData, testData)
 ### Set the column names for the combined datasets
 colnames(combineData) <- c("Subject", "Activity", selectedFeature)
 
-### Trun the activiteis and 
+### Trun the activiteis and subjects to factor
 combineData$Activity <- factor(combineData$Activity, levels = activities[,1], labels = activities[,2])
 combineData$Subject <- as.factor(combineData$Subject)
 
+### Transform the all columns to rows with basis of Subject and Activity 
+combineData.melt <- melt(combineData, id = c("Subject", "Activity"))
+
+### Cast the previous melted data on the basis of Subject and Activity and calulating the mean for each variables 
+combineData.mean <- dcast(combineData.melt, Subject + Activity ~ variable, mean)
+
+### Permanently store the cleanded data to file.
+write.table(combineData.mean, "tidyData.txt", row.names = FALSE, quote = FALSE)
